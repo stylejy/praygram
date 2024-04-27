e31d35129767603c5d6d401d6a6cc0a4c2d0f9dc
JooYoung Lee <stylejy@gmail.com>
2024-04-27 13:18:55 +0900

    UI 기본 구성

---
+++ app/[groupId]/parycard.tsx
@@ -0,0 +1,9 @@
+interface Props {}
+
+export default function Praycard(props: Props) {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-fit bg-white/70 rounded-3xl shadow-md backdrop-blur-lg p-5">
+      testtest
+    </div>
+  );
+}
